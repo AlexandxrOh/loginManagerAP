df571b388eca1a0381d1db31bdce76478456a885
Alex Oh <90042873+AlexandxrOh@users.noreply.github.com>
2023-03-29 22:47:57 -0700

    json Reading and Writing support

--- main.py
+++ main.py
@@ -9,20 +9,50 @@
 # GitHub by GitHub Inc.; Git by Linus Torvalds, Junio C Hamano | Open Source Repository Sync & Version control
 # --- 
 
-usernameList = [] # Holds all saved logins
-passwordList = []
-loginNameList = []
+import os
+import json
 
-def homeScreen():
-    print("\n\nCommand Line Login Manager\nAuthor: Alexander Oh | License: MIT Open Source\n\nNote: Do not use this login manager, this is a demonstration of learning for a AP Computer Science final. I am not responible for dumb mistakes.\n\n| New Login (n) | Login List (l) |")
-    prompt = input("> ")
-    if(prompt == "n"):
-        print("\n\n[NEW LOGIN]")
-        loginName = input("| Login Name: ")
-        userName = input("| Username: ")
-        passWord = input("| Password: ")
-        
-    elif(prompt == "l"):
-        return "list"
-    else: 
-        return "repeat"
+letter_list, upper_letter_list = [list("abcdefghijklmnopqrstuvwxyz")], [list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")]
+number_list = [list("0123456789")]
+symbol_list = [list("!?,./\|@#$%^&*()-_=+:;")]
+
+class login_entry: 
+    def __init__(self, login, username, password): 
+        self.login = login
+        self.username = username
+        self.password = password
+    
+    def __str__(self): # String Return if login_entry is called, mostly used for debugging
+        return f"Login: {self.login}, Username: {self.username}, Password: {self.password}"
+    
+# set debug_json to 'True' to enter a example login into the 'logins.json' file; CAUTION: WILL OVERRIDE 'logins.lson' FILE
+debug_json = True
+if(debug_json == True):
+    print("Debug Entry Appended to 'logins.json'...")
+    debug_dict = {
+        "logins": [
+            {
+                "login": "testLogin",
+                "username": "testUsername",
+                "password": "testPassword"
+            }
+        ]
+    }
+    debug_dict_json = json.dumps(debug_dict, indent=2)
+    with open("logins.json", "w") as l:
+        l.write(debug_dict_json)
+
+if(os.path.exists("logins.json")): # Check if a login.json file exists
+    print("Login json Detected...")
+    with open("logins.json", "r") as l: # Loads json file and exports it into a readable dictionary for python
+        login_dict = json.loads(l.read())
+else: 
+    open("logins.json", "w")
+
+print("\nCommand Line Login Manager\nAuthor: Alexander Oh | License: MIT Open Source\n| New Login (n) | Login List (l) |")
+prompt = input(">")
+if(prompt == "n"):
+    print("\nNew Login Entry")
+    new_login = input("| Login Name: ") 
+    new_user = input("| Username: ")
+    # Password will go here
