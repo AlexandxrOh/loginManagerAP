4d119586db949923433e25d89c0e1d67a5ea2b74
Alex Oh <90042873+AlexandxrOh@users.noreply.github.com>
2023-04-01 13:56:24 -0700

    Startup Functionality

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Login Manager by Alexander Oh 
-# MIT License
-
 # --- Sources --- #
 # Visual Studio Code by Microsoft | IDE used to program code
 # Python 3.11.2 by Python Software Foundation | Programming Language used
@@ -9,24 +6,34 @@
 # GitHub by GitHub Inc.; Git by Linus Torvalds, Junio C Hamano | Open Source Repository Sync & Version control
 # --- 
 
+# make preferences functionality
+
+
 import os
 import json
 
+name = ""
+
+# Preferences File creation
+if(os.path.exists("config.txt") == False):
+    print("ERROR: config.txt failed to load")
+    open("config.txt", "w")
+    print("config.txt created...")
+
 if(os.path.exists("logins.json")): # Check if a login.json file exists
-    print("Login json Detected...")
+    print("Logins.json Detected...")
 else: 
     open("logins.json", "w") # if 'lgons.json' doesn't exist, create it and insert dictionary format
-    print("Login json not detected...")
+    print("ERROR: logins.json not detected")
     format_dict = {"logins": []}
     format_dict_json = json.dumps(format_dict, indent=2)
     with open("logins.json", "r+") as l:
         l.write(format_dict_json)
-    print("logins.json created! ")
+    print("logins.json created... ")
 
 with open("logins.json", "r") as l: # Loads json file and exports it into a readable dictionary for python
     login_dict = json.loads(l.read())
-print("logins.json loaded...")
-
+print("logins.json loaded...\nLogins imported successfully!")
 
 class login_entry: 
     def __init__(self, login, username, password): 
@@ -51,13 +58,18 @@ def write_to_json(entry):
         l.write(login_dict_json)
 
 def list_all():
+    print("\n| " + name + "'s Logins")
     for x in range(len(login_dict["logins"])):
+        print("\nLogin " + str(x + 1) + ":")
         print(login_dict["logins"][x]["login"])
+        print(login_dict["logins"][x]["username"])
+        print(login_dict["logins"][x]["password"])
+
 
 # set debug_json to 'True' to enter a example login into the 'logins.json' file; CAUTION: WILL OVERRIDE 'logins.lson' FILE
 debug_json = True
 if(debug_json):
-    print("Debug Entry Appended to 'logins.json'...")
+    print("DEBUG: Entry Appended to 'logins.json'...")
     debug_dict = {
         "logins": [
             {
